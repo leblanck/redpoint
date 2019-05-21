8f1700925074849159d738e86d6fe64fadd2b29b
Kyle LeBlanc <kyle.paul.leblanc@gmail.com>
2019-05-21 13:54:48 -0400

    shell setup update
    Added section for updating local shell preferences (Install iTerm, zsh, themes, etc)

--- bustIT.sh
+++ bustIT.sh
@@ -31,7 +31,7 @@ caskInstaller() {
   # This will iterate through cask installs, only stopping when a trigger is hit to execute
   # additional commands for that specific installer.
   # To add additional casks, add them into $casks array.
-  declare -a casks=("spectacle" "wireshark" "clipy" "atom" "slack" "github" "zeplin" "adobe-creative-cloud" "dozer" "figma" "messenger" "dropbox" "sketch" "xnviewmp" "fliqlo")
+  declare -a casks=("spectacle" "wireshark" "clipy" "atom" "slack" "github" "iterm2" "zeplin" "dozer" "figma" "messenger" "dropbox" "sketch" "xnviewmp" "fliqlo")
   atomTrigger="atom"
   adobeTrigger="adobe-creative-cloud"
 
@@ -72,6 +72,47 @@ atomExtras() {
   #CLI Tools
   echo "`date` Installing Atom CLI Tools..."
   ln -s /Applications/Atom.app/Contents/Resources/app/atom.sh /usr/local/bin/atom
+}
+
+shellSetup() {
+  #The following will start the setup for local shell preferences (zsh)
+  #iTerm2 is installed in the cask list in the caskInstaller function
+  
+  #Do we have git installed?
+  if [[ $(command -v git) == "" ]]; then
+    echo "Installing git..."
+    git
+  else
+    echo "Git is installed; Running version: "
+    git --version
+  fi
+  
+  #Install oh-my-zsh
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+  #Download iTerm theme
+  # Set Theme by iTerm2 > Preferences > Profiles > Colors > Color Presets > Import - Import the halcyon.itermcolors file
+  curl -L -o ~/halcyon.itermcolors  https://raw.githubusercontent.com/bchiang7/halcyon-iterm/master/halcyon.itermcolors
+
+  #Install Powerline Font pack
+  mkdir ~/repos
+  cd ~/repos
+  git clone https://github.com/powerline/fonts.git --depth=1
+  cd fonts
+  ./install.sh
+  cd ..
+  rm -rf fonts
+  
+  #The following is option/personal preference (to be scripted eventually):
+  ## Set Font and Non-ASCII font to Hack 12pt Reg (iTerm2 > Preferences > Profiles > Text)
+  ## Add alias's to ~/.zshrc
+  ##    alias home="cd ~/"
+  ##    alias ls="ls -la"
+  ## Add plugins to ~/.zshrc
+  ##    plugins=(git extract osx)
+  ## Change zsh prompt theme in ~/.zshrc to agnoster (ZSH_THEME="agnoster")
+  ## Change agnoster theme prompt context (~/.oh-my-zsh/themes/agnoster.zsh-theme : line 92) to the following:
+  ##    prompt_segment black default "%(!.%{%F{yellow}%}.)this ➡"
 }
 
 localMacOSSetup() {
@@ -125,6 +166,12 @@ homebrewInstall() {
     sleep 5
     echo "`date` Installing Firebase..."
     npm install -g firebase-tools
+    sleep 5
+    echo "`date` Setting up shell preferences..."
+    shellSetup
+    sleep 5
+    echo "`date` Installing CLI Trash..."
+    npm install --global trash-cli
     sleep 5
     echo "`date` Setting up macOS local preferences..."
     localMacOSSetup
